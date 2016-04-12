dc2ceb40ef5cf076abc8bac33b13624c6fe4b32c
Marcus Janke <mj@marcusjanke.de>
2016-04-12 21:30:56 +0200

    added simple Spring REST example

---
+++ spring-rest/src/main/java/de/marcusjanke/examples/springrest/model/Echo.java
@@ -0,0 +1,20 @@
+package de.marcusjanke.examples.springrest.model;
+
+public class Echo {
+
+	private final long id;
+	private final String content;
+
+	public Echo(long id, String content) {
+		this.id = id;
+		this.content = content;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+}
